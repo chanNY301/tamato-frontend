494c26a5a7d601378b1e09a7e6db18dbdd1d6158
wanghandan <1939366582@qq.com>
2026-01-12 16:02:59 +0800

    图标和标题修改

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      title: 'Tomato'
+    }
+  },
   devServer: {
     port: 8080,
     host: 'localhost',
